25daf39cc8336a12bdd3fe2d7da96a0c153dedb8
e115763 <e115763@gmail.com>
2012-10-09 15:33:27 +0900

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ln -s `pwd`/.vimrc ~/.vimrc
+ln -s `pwd`/.vimshrc ~/.vimshrc
+ln -s `pwd`/.vim ~/.vim
+
+ln -s `pwd`/.zshrc ~/.zshrc
+ln -s `pwd`/.screenrc ~/.screenrc
+ln -s `pwd`/.vrapperrc ~/.vrapperrc
+ln -s `pwd`/.vimperatorrc ~/.vimperatorrc
