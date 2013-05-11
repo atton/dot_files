108500e19964750e131fccd92f770d6a1f6de189
e115763 <e115763@gmail.com>
2013-05-11 16:26:46 +0900

    add agda-mode settings

--- dot_files/.emacs.d/init.el
+++ dot_files/.emacs.d/init.el
@@ -1,1 +1,7 @@
 (global-set-key "\C-h" 'delete-backward-char)
+
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+
+; agda-mode
+(load-file (let ((coding-system-for-read 'utf-8))
+                (shell-command-to-string "agda-mode locate")))
