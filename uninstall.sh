6914140bca4ccfaf75e77c57c1f0d5c63e22b21b
e115763 <e115763@gmail.com>
2013-06-10 10:24:23 +0900

    improve dot files unlink script. check symlink

--- uninstall.sh
+++ uninstall.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-# unlink script : files in scripts unlink from /usr/local/bin
+# dot files unlink script : unlink dot files from $HOME
 
 files=`ls -1A dot_files`
 target_dir=$HOME
 
 for file in $files
 do
-    command="rm -f ${target_dir}/${file}"
+    target=${target_dir}/${file} 
+
+    if [ -L $target ]; then 
+        # if exists as symlink
+        command="rm -f $target"
+    elif [ -e $target ]; then
+        # if exists not symlink
+        command="mkdir -p backup && mv ${target} backup"
+    fi
+
     echo $command
     $command
 done
