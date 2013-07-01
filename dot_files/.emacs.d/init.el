d6351389f806988eec20fd27e49b1b2f5e0d9a2a
e115763 <e115763@gmail.com>
2013-07-01 16:56:50 +0900

    add load-path to agda standard library

--- dot_files/.emacs.d/init.el
+++ dot_files/.emacs.d/init.el
@@ -5,3 +5,5 @@
 ; agda-mode
 (load-file (let ((coding-system-for-read 'utf-8))
                 (shell-command-to-string "agda-mode locate")))
+
+(setq agda2-include-dirs (list "." (expand-file-name "~/Library/Agda2/current/src")))
