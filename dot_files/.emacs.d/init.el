f98cc87fef8afa1b16ffb7969305447491118b20
e115763 <e115763@gmail.com>
2014-03-11 11:10:37 +0900

    Add settings east asian ambiguous to emacs

--- dot_files/.emacs.d/init.el
+++ dot_files/.emacs.d/init.el
@@ -2,8 +2,11 @@
 
 (setq load-path (cons "~/.emacs.d/elisp" load-path))
 
+(load "east-asian-ambiguous")
+(set-east-asian-ambiguous-width 2)
+
 ; agda-mode
 (load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
+             (shell-command-to-string "agda-mode locate")))
 
 (setq agda2-include-dirs (list "." (expand-file-name "~/Library/Agda2/current/src")))
