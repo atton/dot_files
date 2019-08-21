ed50e86ef73e08df6775c53cabdc936290d7ddf3
atton <e115763@gmail.com>
2019-08-21 09:52:28 +0900

    Fix node.sh

--- node.sh
+++ node.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-cd `dirname $0`
 
-# npm ls -g --depth=0   # get installed package information
+# $ npm ls -g --depth=0
+# get installed package information
 
-npm install -g yarn
-npm install -g typescript
 npm install -g neovim
+npm install -g npm
+npm install -g typescript
+npm install -g yarn
