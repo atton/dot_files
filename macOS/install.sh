58a39e176cc3a5610fdf1c1abed35dd9b31d4fe3
atton <e115763@gmail.com>
2019-10-10 12:35:01 +0900

    Add macOS KeyBind

---
+++ macOS/install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd `dirname $0`
+
+mkdir -p ~/Library/KeyBindings
+cp DefaultKeyBinding.dict ~/Library/KeyBindings
---
+++ macOS/uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd `dirname $0`
+
+rm ~/Library/KeyBindings/DefaultKeyBinding.dict
+rmdir ~/Library/KeyBindings
