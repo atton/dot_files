375db2ee2060476e4170f1f7cf4a96114ce1ab6f
atton <e115763@gmail.com>
2019-12-14 11:06:50 +0900

    Update python-init.sh

--- scripts/python-init.sh
+++ scripts/python-init.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 echo '----- install packages python3 -----'
-function pip3_install() {
+pip3_install() {
     pip3 install --upgrade-strategy eager --progress-bar off --upgrade $1
-    if [ $? -ne 0 ]; then
-        pip3 install --upgrade-strategy eager --upgrade $1
-    fi
-
     pip3 check $1
 }
 
-pip3_install pynvim
+if [ `uname` == 'Darwin' ]; then
+    # macOS only
+    pip3_install pynvim
+    exit 0
+fi
 
-# not macOS only
-if [ `uname` == 'Darwin' ]; then exit 0; fi
 
-pip3_install awscli
+pip3 install --upgrade pip
+
 pip3_install aws-sam-cli
+pip3_install awscli
+pip3_install pynvim
