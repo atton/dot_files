5118d6701c69af84ddb0403f6b518bcdb7b078af
atton <e115763@gmail.com>
2019-12-15 13:07:39 +0900

    Mini fix

--- scripts/node-init.sh
+++ scripts/node-init.sh
@@ -6,10 +6,11 @@ if which nodenv >& /dev/null; then
     echo "cache = $prefix/.npm" > "$prefix/lib/node_modules/npm/npmrc"
 fi
 
-# $ npm ls -g --depth=0
-# get installed package information
-npm install -g neovim
+# $ npm ls -g --depth=0 # get installed package information
 npm install -g npm
+
+npm install -g neovim
 npm install -g typescript
 npm install -g yarn
+
 if which nodenv >& /dev/null; then nodenv rehash ; fi
