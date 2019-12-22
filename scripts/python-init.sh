cef13d030e17e15b62154ac579780f64893dd216
atton <e115763@gmail.com>
2019-12-22 12:22:03 +0900

    Check availability "pip3" in python-init.sh

--- scripts/python-init.sh
+++ scripts/python-init.sh
@@ -1,20 +1,15 @@
 #!/bin/sh -eux
 
-echo '----- install packages python3 -----'
 pip3_install() {
     pip3 install --upgrade-strategy eager --progress-bar off --upgrade $1
     pip3 check $1
 }
+which pip3
 
-if [ `uname` == 'Darwin' ]; then
-    # macOS only
-    pip3_install pynvim
-    exit 0
-fi
+if [ `uname` == 'Darwin' ]; then pip3_install pynvim ; exit 0 ; fi
 
 
 pip3 install --upgrade setuptools pip
-
 pip3_install aws-sam-cli
 pip3_install awscli
 pip3_install pynvim
