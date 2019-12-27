a380aaa3bb243588d19a18e803beaabfeabb1233
atton <e115763@gmail.com>
2019-12-28 08:35:35 +0900

    node-init.sh supports environment without nodenv

--- scripts/node-init.sh
+++ scripts/node-init.sh
@@ -1,16 +1,36 @@
-#!/bin/sh -eux
+#!/bin/sh
 
-if which nodenv >& /dev/null; then
+nodenv_before_install() {
     nodenv rehash
     prefix=$(nodenv prefix)
     echo "cache = $prefix/.npm" > "$prefix/lib/node_modules/npm/npmrc"
+}
+
+nodenv_after_install() {
+    nodenv rehash
+}
+
+install_packages() {
+    npm install -g npm
+
+    npm install -g neovim
+    npm install -g typescript
+    npm install -g yarn
+    # $ npm ls -g --depth=0 # get installed packages information
+}
+
+which nodenv >& /dev/null;
+check_nodenv=$?
+
+set -eux
+
+which node
+which npm
+
+if [ $check_nodenv -eq 0 ]; then
+    nodenv_before_install
+    install_packages
+    nodenv_after_install
+else
+    install_packages
 fi
-
-# $ npm ls -g --depth=0 # get installed package information
-npm install -g npm
-
-npm install -g neovim
-npm install -g typescript
-npm install -g yarn
-
-if which nodenv >& /dev/null; then nodenv rehash ; fi
