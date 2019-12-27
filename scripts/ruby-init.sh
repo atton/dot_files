ea1252f6e6cd5230f84d4c09548156dc0f8ec43c
atton <e115763@gmail.com>
2019-12-28 08:46:46 +0900

    Adjust ruby-init.sh

--- scripts/ruby-init.sh
+++ scripts/ruby-init.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-if which rbenv >& /dev/null; then rbenv rehash ; fi
-echo ------------- versions -------------
-echo "ruby: `ruby -v`"
-echo "gem:  `gem -v`"
-echo ---------- initializations ----------
-if [ -f Gemfile ]; then echo 'Gemfile detected, abort.'; exit 1; fi
-if [ -f Gemfile.lock ]; then echo 'Gemfile.lock detected, abort.'; exit 1; fi
 export GEM_SPEC_CACHE=$HOME/.config/gem/specs
-set -x
+export BUNDLE_USER_HOME=$HOME/.config/bundler
+set -eux
+which ruby
+which gem
+set +eu
+
+if which rbenv >& /dev/null; then rbenv rehash ; fi
 gem update --no-document --system
 gem install --no-document -f bundler
-cp `dirname $0`/../Gemfile .
-BUNDLE_USER_HOME=~/.config/bundler bundle install
-rm Gemfile*
+bundle install --gemfile "`dirname $0`/../Gemfile"
 if which rbenv >& /dev/null; then rbenv rehash ; fi
