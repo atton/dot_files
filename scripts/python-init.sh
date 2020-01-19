91546957648f084cecf7bfcc4f65097da4d19270
atton <e115763@gmail.com>
2020-01-19 10:45:32 +0900

    Adjust pip3 install option

--- scripts/python-init.sh
+++ scripts/python-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
 
 pip3_install() {
-    pip3 install --upgrade-strategy eager --progress-bar off --upgrade $1
+    pip3 install --no-color --progress-bar off --upgrade $1
     pip3 check $1
 }
 which pip3
@@ -9,7 +9,7 @@ which pip3
 if [ `uname` == 'Darwin' ]; then pip3_install pynvim ; exit 0 ; fi
 
 
-pip3 install --upgrade setuptools pip
+pip3 install --no-color --upgrade setuptools pip
 pip3_install pynvim
 
 
