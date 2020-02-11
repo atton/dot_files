83e559fe682e69aeba7d54b22bb9e2785f3569da
atton <e115763@gmail.com>
2020-02-11 11:38:07 +0900

    Adjust python-init.sh

--- scripts/python-init.sh
+++ scripts/python-init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -eux
 
 pip3_install() {
-    pip3 install --no-color --progress-bar off --upgrade $1
-    pip3 check $1
+    pip3 install --no-color --progress-bar off --upgrade $@
+    pip3 check $@
 }
 pip3 --version
 
@@ -11,5 +11,4 @@ pip3 check setuptools pip
 pip3_install pynvim
 
 if [ $# -eq 1 ]; then if [ $1 = 'NO_AWS' ]; then exit 0; fi fi
-pip3_install awscli
-pip3_install aws-sam-cli
+pip3_install awscli aws-sam-cli
