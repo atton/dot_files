0f20c7fd6d1ab1c3d2a584e8bcf3be56eb3759c9
atton <e115763@gmail.com>
2020-02-13 08:45:58 +0900

    Resolve parse error in python-init.sh

--- scripts/python-init.sh
+++ scripts/python-init.sh
@@ -10,5 +10,5 @@ pip3 install --upgrade setuptools pip
 pip3 check setuptools pip
 pip3_install pynvim
 
-if [ $# -eq 1 ]; then if [ $1 = 'NO_AWS' ]; then exit 0; fi fi
+if [ $# -eq 1 ]; then if [ $1 = 'NO_AWS' ]; then exit 0; fi; fi
 pip3_install awscli aws-sam-cli
