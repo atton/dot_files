65105817fa08ad608db5bf06d896faa9210e7212
atton <e115763@gmail.com>
2020-02-19 08:33:31 +0900

    Add serverless package by node

--- scripts/node-init.sh
+++ scripts/node-init.sh
@@ -11,12 +11,8 @@ nodenv_after_install() {
 }
 
 install_packages() {
-    npm install -g npm
-
-    npm install -g neovim
-    npm install -g typescript
-    npm install -g yarn
-    # $ npm ls -g --depth=0 # get installed packages information
+    npm update -g npm
+    npm install -g neovim serverless typescript yarn
 }
 
 which nodenv >& /dev/null;
