7c26c165e2339f71147a2d2f2df077a42ea5ff47
atton <e115763@gmail.com>
2020-02-20 11:44:16 +0900

    Mini fix

--- scripts/ruby-init.sh
+++ scripts/ruby-init.sh
@@ -3,8 +3,8 @@
 export GEM_SPEC_CACHE=$HOME/.config/gem/specs
 export BUNDLE_USER_HOME=$HOME/.config/bundler
 
-set -eux
 echo '--- before bundle install ---'
+set -eux
 which ruby
 which gem
 set +eu
@@ -15,8 +15,8 @@ gem install --no-document -f bundler
 bundle install --gemfile "`dirname $0`/../Gemfile"
 if which rbenv >& /dev/null; then rbenv rehash ; fi
 
-set -eu
 echo '--- after bundle install ---'
+set -eu
 which kramdown
 which neovim-ruby-host
 which pry
