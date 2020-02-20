0286bdca06da059e50393c3eecb3c4aae99063eb
atton <e115763@gmail.com>
2020-02-20 12:15:39 +0900

    Adjust ruby-init.sh

--- scripts/ruby-init.sh
+++ scripts/ruby-init.sh
@@ -3,20 +3,19 @@
 export GEM_SPEC_CACHE=$HOME/.config/gem/specs
 export BUNDLE_USER_HOME=$HOME/.config/bundler
 
-echo '--- before bundle install ---'
 set -eux
-which ruby
-which gem
-set +eu
+ruby --version
+gem --version
 
+set +eu
 if which rbenv >& /dev/null; then rbenv rehash ; fi
 gem update --no-document --system
 gem install --no-document -f bundler
 bundle install --gemfile "`dirname $0`/../Gemfile"
 if which rbenv >& /dev/null; then rbenv rehash ; fi
 
-echo '--- after bundle install ---'
 set -eu
-which kramdown
-which neovim-ruby-host
-which pry
+gem --version
+kramdown --version
+neovim-ruby-host --version
+pry --version
