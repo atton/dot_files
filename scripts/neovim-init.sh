bdcdbd9fe4a13c68e3c114ab180998fcd48243f8
atton <e115763@gmail.com>
2020-02-22 09:05:46 +0900

    Adjust neovim-init.sh for "neovim-*" jobs

--- scripts/neovim-init.sh
+++ scripts/neovim-init.sh
@@ -2,15 +2,26 @@
 cd `dirname $0`
 
 set -eux
+nvim --version
 
-which nvim curl git sh pip3 gcc
-./python-init.sh 'NO_AWS'
+which bash curl git bash pip3 gcc
+bash python-init.sh 'NO_AWS'
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > /tmp/installer.sh
-sh /tmp/installer.sh ~/.config/nvim/dein
-rm /tmp/installer.sh
+installer_path='/tmp/installer.sh'
+curl -s https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > $installer_path
+bash $installer_path $HOME/.config/nvim/dein
+rm $installer_path
 
-mkdir -p ~/.config/nvim/eskk
-curl https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L > ~/.config/nvim/eskk/SKK-JISYO.L
+mkdir -p $HOME/.config/nvim/eskk
+curl -s https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L > $HOME/.config/nvim/eskk/SKK-JISYO.L
 
 nvim -c ':call dein#install()' -c ':UpdateRemotePlugins' -c ':quit'
+test -d $HOME/.config/nvim/dein/repos/github.com/atton/gundo.vim
+test -d $HOME/.config/nvim/dein/repos/github.com/Shougo/dein.vim
+test -d $HOME/.config/nvim/dein/repos/github.com/Shougo/deoplete.nvim
+test -f $HOME/.config/nvim/eskk/SKK-JISYO.L
+
+checkhealth_path='/tmp/checkhealth.txt'
+nvim -c ':checkhealth' -c ":write $checkhealth_path" -c ':quitall'
+cat $checkhealth_path
+rm $checkhealth_path
