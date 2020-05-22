413271e2d0ff00bfae0726e4bb6a9cd7f0566b2f
atton <e115763@gmail.com>
2020-05-22 20:26:33 +0900

    Adjust "docker-init.sh"

--- scripts/docker-init.sh
+++ scripts/docker-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -eux
-cd `dirname $0`
 
 docker pull alpine
 docker pull centos
-docker pull ruby:2.5.8-alpine
-docker pull ruby:2.7.1-alpine
+
+docker pull atton/latex-make
+docker pull atton/webpage-title
